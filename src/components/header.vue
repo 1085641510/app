db24666c99bfde15db349cf291c3f046a9fdbea2
1085641510 <1085641510@qq.com>
2018-08-10 17:22:31 +0800

    Add files via upload

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,13 +1,51 @@
 <template>
-  <div class="header">
-        王鹏强的博客
-        <span class="he">2131231</span>
+  <div :class='headerClass'>
+        <div :class="leftIconClass" v-html="leftIcon"></div>
+        <div class="title" :class="titleClass">{{title}}</div>
+        <div :class="rightIconClass" v-html="rightIcon"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'bgHeader',
+  props:{
+      title:{
+          default: 'my blog',
+          type: null,
+          required: false
+      },
+      titleClass:{
+          default: 'font-title',
+          type: null,
+          required: false
+      },
+      leftIcon:{
+          default: '',
+          type: null,
+          required: false
+      },
+      rightIcon:{
+          default: '',
+          type: null,
+          required: false
+      },
+      leftIconClass:{
+          default: 'left-icon',
+          type: String,
+          required: false
+      },
+      rightIconClass:{
+          default: 'right-icon',
+          type: String,
+          required: false
+      },
+      headerClass:{
+          default: 'header',
+          type: String,
+          required: false
+      }
+  },
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
@@ -17,13 +55,35 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
+    $baseFontSize :75px;
+
+    @function torem($px){
+        @return $px / $baseFontSize * 1rem; 
+    }
+
     .header{
         position: fixed;
         width: 100%;
         background: #222222;
         color: #fff;
-        font-size: 18px;
-        height: 50px;
+        font-size: torem(28px);
+        height: torem(80px);
+        display: flex;
+        align-items: center;
+        padding: 0 torem(20px);;
+    }
+    .font-title{
+        color: #fff;
+        font-size: torem(28px);
+    }
+    .title{
+        flex: 1;
+        text-align: center;
+    }
+    .left-icon,.right-icon{
+        width: torem(50px);
+        height: torem(50px);
+        font-size: torem(50px);
     }
 </style>
