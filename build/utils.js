917dff92a37b03cfd77268ff323fb3b7e6e60750
1085641510 <1085641510@qq.com>
2018-08-10 15:21:38 +0800

    Add files via upload

--- build/utils.js
+++ build/utils.js
@@ -28,7 +28,13 @@ exports.cssLoaders = function (options) {
       sourceMap: options.sourceMap
     }
   }
-
+  // 自动转换px为rem
+  const px2remLoader = {
+    loader: 'px2rem-loader',
+    options: {
+      remUnit: 75
+    }
+  }
   // generate loader string to be used with extract text plugin
   function generateLoaders (loader, loaderOptions) {
     const loaders = options.usePostCSS ? [cssLoader, postcssLoader] : [cssLoader]
